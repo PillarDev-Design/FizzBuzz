3c74f76d14b1fa7a68d2696e733bc34598acbdb7
Christopher Nabors <PillarDD@Gmail.com>
2012-10-09 19:30:29 -0400

    Revised code

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,12 +3,12 @@ x = 1
 
 # Function to print (Strictly just a formatting idea)
 def print_x(number, word):
-    if (number < 10):
-        print '(00%s) %s' % (number, word)
-    if (number < 100):
-        print '(0%s) %s' % (number, word)
     if (number == 100):
         print '(%s) %s' % (number, word)
+    elif (number < 10):
+        print '(00%s) %s' % (number, word)
+    elif (number < 100):
+        print '(0%s) %s' % (number, word)
 
 # Loop Until 100
 while x < 101:

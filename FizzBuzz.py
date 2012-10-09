896ab960470479f436005301f8892ef0ab45d4f9
Christopher Nabors <PillarDD@Gmail.com>
2012-10-09 19:17:31 -0400

    Fixed code - accidently left excess parts

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,27 +1,33 @@
+# Declare Variable
 x = 1
 
+# Loop Until 100
 while x < 101:
-    if (x%3==0) and (x%5==0):
+    # Divisible by 3 and 5?
+    if (x%3 == 0) and (x%5 == 0):
         if (x < 10):
             print '(00%s) FizzBuzz' % (x)
         elif (x < 100):
             print '(0%s) FizzBuzz' % (x)
         elif (x == 100):
             print '(%s) FizzBuzz' % (x)
-    elif x%3==0:
+    # Divisible by 3?
+    elif x%3 == 0:
         if (x < 10):
             print '(00%s) Fizz' % (x)
         elif (x < 100):
             print '(0%s) Fizz' % (x)
         elif (x == 100):
             print '(%s) Fizz' % (x)
-    elif x%5==0:
+    # Divisible by 5?
+    elif x%5 == 0:
         if (x < 10):
             print '(00%s) Buzz' % (x)
         elif (x < 100):
             print '(0%s) Buzz' % (x)
         elif (x == 100):
             print '(%s) Buzz' % (x)
+    # Not divisible by 3 or 5
     elif x < 101:
         if (x < 10):
             print '(00%s)' % (x)
@@ -30,6 +36,5 @@ while x < 101:
         elif (x == 100):
             print '(%s)' % (x)
 
+    # Increment the Variable
     x += 1
-    if (x == 101):
-        done = True
